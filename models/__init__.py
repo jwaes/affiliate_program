c5ffad2252ac7d68a49204111d122ac40ce0a223
jaco <jaco.waes@gmail.com>
2024-11-04 15:10:34 +0100

    init

--- models/__init__.py
+++ models/__init__.py
@@ -3,6 +3,4 @@ from . import affiliate_affiliate
 from . import link_tracker
 from . import sale_order
 from . import sale_report
-from . import website_visitor
-from . import crm_lead
 from . import res_partner
